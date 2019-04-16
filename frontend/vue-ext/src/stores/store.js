4b49c5125b97e1ac86f90fb79d11c81d7ebdf1bb
wlvstv <mike@wlvs.tv>
2019-04-16 02:17:03 -0400

    - Added Modules folder for vuex modules (#2)
    - Modified store to include modules and imported extension module
    - Modified extension module to be an object, added namspacing
    - Modified extension module, added state and mutations for existing actions

--- frontend/vue-ext/src/stores/store.js
+++ frontend/vue-ext/src/stores/store.js
@@ -1,16 +1,11 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import extension from "./modules/extension";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  modules: {
+    extension
+  }
 });
